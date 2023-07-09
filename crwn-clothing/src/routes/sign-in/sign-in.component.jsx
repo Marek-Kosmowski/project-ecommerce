b3f5c558a23ee25716ea4cf35ba10b81e649976a
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-07-09 21:48:51 +0200

    Create form inside sign-up-form.component.jsx

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
 // import {auth, } from '../../utils/firebase/firebase';
-
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
   // wherever you make a call to the database it is going to be asynchronous
@@ -16,6 +16,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
